ff9acb40c6160cdd36604112d92d9ec425b9408f
Vedant Gupta <vedugpt77@gmail.com>
2025-10-12 12:06:43 -0400

    added docstrings

--- product_assistant/etl/data_scrapper.py
+++ product_assistant/etl/data_scrapper.py
@@ -10,15 +10,26 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 class FlipkartScrapper:
     def __init__(self, output_dir='data'):
+        '''
+        Initialize the FlipkartScrapper class.
+        '''
         self.output_dir = output_dir
         os.makedirs(self.output_dir, exist_ok=True)
 
     def get_top_reviews(self, product_url, count=2):
+        '''
+        Get the top reviews for a given product URL.
+        '''
         pass
 
     def scrape_flipkart_product(self, query, max_products=1, review_count=2):
+        '''
+        Scrape Flipkart for products matching the query and get their reviews.
+        '''
         pass
 
     def save_to_csv(self, data, filename='product_reviews.csv'):
+        '''
+        Save the scraped data to a CSV file.
+        '''
         pass
-    
